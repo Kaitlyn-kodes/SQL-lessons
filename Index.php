eaa8b854fc6e1782d1640d2937640a69fdb4af71
Kaitlyn-kodes <Kaitlynellis29@gmail.com>
2024-10-02 08:23:57 -0500

    fgm

--- Index.php
+++ Index.php
@@ -10,20 +10,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+  <form action="includes/signup.inc.php" method="POST">
+        <input type="text" name="first" placeholder="First Name">
+        <br>
+        <input type="text" name="last" placeholder="Last Name">
+        <br>
+        <input type="text" name="email" placeholder="E-mail">
+        <br>
+        <input type="text" name="uid" placeholder="Username">
+        <br>
+        <input type="text" name="pwd" placeholder="Password">
+        <br>
+        <button type="submit" name="submit"> Sign Up</button>
+    </form>
 
-    <?php 
 
-      $sql = "SELECT * FROM usersfull;";
-      $results = mysqli_query($conn, $sql);
-      $resultCheck = mysqli_num_rows($result);
-
-      if ($resultCheck > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          echo $row ['user_uid'];
-        } 
-      }
-    ?>
-
+   
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
